144b1ed7e899ec66f805efe5d418401af8a3567d
Danish War Ramani <enoughdanish@gmail.com>
2026-08-21 18:46:20 +0530

    Time: 1 ms (99.66%), Space: 42.5 MB (96.79%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i = 0; i < stones.length; i++){
+            pq.add(stones[i]);
+        }
+
+        while(pq.size() > 1){
+            int y = pq.poll();
+            int x = pq.poll();
+
+            if(y >= x){
+                pq.add(y - x);
+                
+            }
+            
+        }   
+        return pq.isEmpty() ? 0 : pq.poll();
+
+        
+
+
+    }
+}
